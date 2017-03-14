5b4b10d5c9753bd6df935d379554d8e11766509c
Ravikant Verma <ravikant@blazeautomation.com>
2017-03-14 14:59:46 +0530

    # added database concept

--- app/src/main/java/com/example/ravikant/gcm/Main3Activity.java
+++ app/src/main/java/com/example/ravikant/gcm/Main3Activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,15 +33,15 @@ public class Main3Activity extends AppCompatActivity {
         User user = new User("Ravikant Verma", "ravikant@blazeautomation.com");
         databaseReference.child(userId).setValue(user);
         addUserChangeListener();*/
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView pd = (TextView) findViewById(R.id.password);
-        userId = username.getText().toString().trim();
-        password = pd.getText().toString().trim();
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText pd = (EditText) findViewById(R.id.password);
 
         findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addUserChangeListener();
+                userId = username.getText().toString().trim();
+                password = pd.getText().toString().trim();
+                addGetUserListener();
             }
         });
     }
@@ -50,7 +51,10 @@ public class Main3Activity extends AppCompatActivity {
         final ProgressDialog dialog = new ProgressDialog(this);
         dialog.setMessage("Please wait");
         dialog.show();
-         databaseReference.child(userId).addValueEventListener(new ValueEventListener() {
+        String tmpEmail = userId;
+        tmpEmail = tmpEmail.replace("@", "_");
+        tmpEmail = tmpEmail.replace(".", "_");
+         databaseReference.child(tmpEmail).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
@@ -58,6 +62,34 @@ public class Main3Activity extends AppCompatActivity {
                 // Check for null
                 if (user == null) {
                     Log.e("TAG", "User data is null!");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                dialog.dismiss();
+                Log.e("TAG", "Failed to read user", error.toException());
+            }
+        });
+    }
+
+    private void addGetUserListener() {
+        // User data change listener
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage("Please wait");
+        dialog.show();
+        String tmpEmail = userId;
+        tmpEmail = tmpEmail.replace("@", "_");
+        tmpEmail = tmpEmail.replace(".", "_");
+        databaseReference.child(tmpEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                dialog.dismiss();
+                // Check for null
+                if (user == null) {
+                    Toast.makeText(getApplicationContext(), "Invalid user name", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
